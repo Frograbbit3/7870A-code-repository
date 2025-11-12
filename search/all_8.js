d6123cb6ef01e133ca828b752c2144c7ead52d01
Frograbbit3 <Frograbbit3@users.noreply.github.com>
2025-11-12 16:06:58 +0000

    deploy: b8e1e48158dca83f9997630f835094b848afb45e

--- search/all_8.js
+++ search/all_8.js
@@ -1,7 +1,6 @@
 var searchData=
 [
-  ['drive_0',['drive',['../d4/d52/classDrivetrainLib_1_1Drivetrain.html#ad857862ce67672790ebb0426d4a68b25',1,'DrivetrainLib::Drivetrain']]],
-  ['drive_20auton_20code_1',['Vex Robotics Team 7870A Drive / Auton Code',['../d0/d30/md_README.html',1,'']]],
-  ['drivetrain_2',['Drivetrain',['../d4/d52/classDrivetrainLib_1_1Drivetrain.html',1,'DrivetrainLib']]],
-  ['drivetrainsettings_3',['DrivetrainSettings',['../dd/df7/structDrivetrainEnums_1_1DrivetrainSettings.html',1,'DrivetrainEnums']]]
+  ['drive_20auton_20code_0',['Vex Robotics Team 7870A Drive / Auton Code',['../d0/d30/md_README.html',1,'']]],
+  ['drivetrain_1',['Drivetrain',['../d4/d52/classDrivetrainLib_1_1Drivetrain.html',1,'DrivetrainLib']]],
+  ['drivetrainsettings_2',['DrivetrainSettings',['../dd/df7/structDrivetrainEnums_1_1DrivetrainSettings.html',1,'DrivetrainEnums']]]
 ];
--- search/functions_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['drive_0',['drive',['../d4/d52/classDrivetrainLib_1_1Drivetrain.html#ad857862ce67672790ebb0426d4a68b25',1,'DrivetrainLib::Drivetrain']]]
-];
