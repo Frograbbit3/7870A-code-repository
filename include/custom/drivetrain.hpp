5a9ced5147160a0549ba9caf34e82eddcda62022
moakdoge <nathanie.405793@nv.ccsd.net>
2025-11-17 13:03:37 -0800

    Added non-task based antidrift. Called when .moveDistance is used.

--- include/custom/drivetrain.hpp
+++ include/custom/drivetrain.hpp
@@ -19,6 +19,26 @@ namespace DrivetrainLib
         std::vector<int8_t> leftSide;
         std::vector<int8_t> rightSide;
         pros::Imu* imu;
+        void antiDrift() {
+            while (leftMotors.isMoving() || rightMotors.isMoving())
+            {
+                double wheelRad = DrivetrainEnums::getWheelDiameter(leftMotors.group[0].getWheelType()) / 2.0;
+                double leftDistance = leftMotors.getRotation() * 2 * pi * wheelRad;
+                double rightDistance = rightMotors.getRotation() * 2 * pi * wheelRad;
+                double drift = leftDistance - rightDistance;
+
+                double correction = drift * 0.5; // Tune this factor for your robot
+                double leftVelocity = 100 - correction;  // Base velocity minus correction
+                double rightVelocity = 100 + correction; // Base velocity plus correction
+
+                leftVelocity = minmax<double>(leftVelocity, -127, 127);
+                rightVelocity = minmax<double>(rightVelocity, -127, 127);
+
+                leftMotors.setVelocity(leftVelocity);
+                rightMotors.setVelocity(rightVelocity);
+                pros::delay(10);
+            }
+        }
     public:
         /*Resets all motors & calibrates the gyro.*/
         void calibrate() {
@@ -95,11 +115,7 @@ namespace DrivetrainLib
 
             leftMotors.moveRelative(rotations);
             rightMotors.moveRelative(rotations);
-            while (leftMotors.isMoving() || rightMotors.isMoving())
-            {
-                /* code */
-            }
-            
+            antiDrift();
         }
         /*
             Sets the velocity of both sides. An easier way of calling .setLeftVelocity and .setRightVelocity
