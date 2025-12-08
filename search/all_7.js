078bfc200919eedc1569baf15917bafcc7312862
Frograbbit3 <Frograbbit3@users.noreply.github.com>
2025-12-08 21:59:54 +0000

    deploy: 6ad15db60f0f1d9e088e1bb31671a49521ffc0a6

--- search/all_7.js
+++ search/all_7.js
@@ -4,8 +4,9 @@ var searchData=
   ['code_1',['Vex Robotics Team 7870A Drive / Auton Code',['../d0/d30/md_README.html',1,'']]],
   ['controlbinding_2',['ControlBinding',['../d7/d8f/structMKV5_1_1ControllerInputs_1_1ControlBinding.html',1,'MKV5::ControllerInputs']]],
   ['controller_3',['Controller',['../d0/d30/md_README.html#autotoc_md4',1,'']]],
-  ['controllersettings_4',['ControllerSettings',['../d4/d56/structMKV5_1_1ControllerSettings.html',1,'MKV5']]],
-  ['controlscheme_5',['ControlScheme',['../d7/d97/classMKV5_1_1ControlScheme.html',1,'MKV5']]],
-  ['current_20version_3a_202_200_6',['Current Version: 2.0',['../d0/d30/md_README.html#autotoc_md1',1,'']]],
-  ['custommotor_7',['CustomMotor',['../d5/d4c/structMKV5_1_1CustomMotor.html',1,'MKV5']]]
+  ['controllerscreen_4',['ControllerScreen',['../d8/d7e/structMKV5_1_1ControllerInputs_1_1ControllerScreen.html',1,'MKV5::ControllerInputs']]],
+  ['controllersettings_5',['ControllerSettings',['../d4/d56/structMKV5_1_1ControllerSettings.html',1,'MKV5']]],
+  ['controlscheme_6',['ControlScheme',['../d7/d97/classMKV5_1_1ControlScheme.html',1,'MKV5']]],
+  ['current_20version_3a_202_200_7',['Current Version: 2.0',['../d0/d30/md_README.html#autotoc_md1',1,'']]],
+  ['custommotor_8',['CustomMotor',['../d5/d4c/structMKV5_1_1CustomMotor.html',1,'MKV5']]]
 ];
